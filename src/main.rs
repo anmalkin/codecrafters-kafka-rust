0ebce565ebbe8d3e0ae8bc86607850c64ee60ebc
amalkin <102127060+anmalkin@users.noreply.github.com>
2024-12-09 09:21:58 -0800

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
 #![allow(unused_imports)]
+use std::io::Write;
+use std::net::TcpStream;
 use std::net::TcpListener;
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     println!("Logs from your program will appear here!");
-
-    // Uncomment this block to pass the first stage
 
     let listener = TcpListener::bind("127.0.0.1:9092").unwrap();
 
@@ -13,10 +13,34 @@ fn main() {
         match stream {
             Ok(_stream) => {
                 println!("accepted new connection");
+                handle_connection(_stream);
             }
             Err(e) => {
                 println!("error: {}", e);
             }
         }
     }
+}
+
+pub fn handle_connection(mut stream: TcpStream) {
+    let response: Vec<u8> = vec![0, 0, 0, 0, 0, 0, 0, 7];
+    let _ = stream.write(&response);
+}
+
+#[derive(Debug)]
+pub struct KResponse {
+    message_size: i32,
+    header: Header,
+}
+
+impl KResponse {
+    pub fn new() -> Self {
+        let header = Header { correlation_id: 7 };
+        Self { message_size: 0, header }
+    }
+}
+
+#[derive(Debug)]
+struct Header {
+    correlation_id: i32
 }
